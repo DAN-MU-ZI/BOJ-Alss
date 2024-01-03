a6febb3c6ce61f54b71b72b48b1fd0ce0e538885
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-03 23:27:09 +0900

    [Bronze I] Title: 단어 공부, Time: 528 ms, Memory: 32768 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.nextLine();
+        char[] charArray = s.toCharArray();
+        int[] charCounter = new int[26];
+
+        for (char c : charArray) {
+            if (c < 97) {
+                charCounter[c - 65]++;
+            } else {
+                charCounter[c - 97]++;
+            }
+        }
+
+        char modeChar = 'A';
+        boolean isDuplicated = false;
+        for (int j = 1; j < charCounter.length; j++) {
+            if (charCounter[j] > charCounter[modeChar - 65]) {
+                modeChar = (char) (j + 65);
+                isDuplicated = false;
+            } else if (charCounter[j] == charCounter[modeChar - 65]) {
+                isDuplicated = true;
+            }
+        }
+
+        if (isDuplicated) {
+            System.out.println("?");
+        } else {
+            System.out.println(modeChar);
+        }
+
+    }
+}
