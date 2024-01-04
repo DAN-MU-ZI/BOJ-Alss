8cfd4b54cbac9e86cd2c0fb9259781f33e807480
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-04 21:41:21 +0900

    [Bronze III] Title: 소금 폭탄, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -1,47 +1,44 @@
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.SimpleFormatter;
 import java.util.stream.Collectors;
 
 public class Main {
-    static int StringTimeToSeconds(String string) {
-        List<Integer> times = Arrays.stream(string.split(":"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+	static int StringTimeToSeconds(String string) {
+		List<Integer> times = Arrays.stream(string.split(":"))
+			.map(Integer::parseInt)
+			.collect(Collectors.toList());
 
-        int timestamp;
-        timestamp = times.get(0);
-        timestamp = timestamp * 60 + times.get(1);
-        timestamp = timestamp * 60 + times.get(2);
+		int timestamp;
+		timestamp = times.get(0);
+		timestamp = timestamp * 60 + times.get(1);
+		timestamp = timestamp * 60 + times.get(2);
 
-        return timestamp;
-    }
+		return timestamp;
+	}
 
-    static String SecondsToStringTime(int seconds) {
-        int HH = seconds / (60 * 60);
-        int MM = seconds / 60 % 60;
-        int SS = seconds % 60;
-        return String.format("%02d:%02d:%02d", HH, MM, SS);
-    }
+	static String SecondsToStringTime(int seconds) {
+		int HH = seconds / (60 * 60);
+		int MM = seconds / 60 % 60;
+		int SS = seconds % 60;
+		return String.format("%02d:%02d:%02d", HH, MM, SS);
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        String startTime = scanner.nextLine();
-        String endTime = scanner.nextLine();
+		String startTime = scanner.nextLine();
+		String endTime = scanner.nextLine();
 
-        int startSeconds = StringTimeToSeconds(startTime);
-        int endSeconds = StringTimeToSeconds(endTime);
+		int startSeconds = StringTimeToSeconds(startTime);
+		int endSeconds = StringTimeToSeconds(endTime);
 
-        String string;
-        if (endSeconds > startSeconds) {
-            string = SecondsToStringTime(endSeconds - startSeconds);
-        } else {
-            string = SecondsToStringTime(endSeconds + (24 * 60 * 60) - startSeconds);
-        }
-        System.out.println(string);
-    }
+		String string;
+		if (endSeconds > startSeconds) {
+			string = SecondsToStringTime(endSeconds - startSeconds);
+		} else {
+			string = SecondsToStringTime(endSeconds + (24 * 60 * 60) - startSeconds);
+		}
+		System.out.println(string);
+	}
 }
