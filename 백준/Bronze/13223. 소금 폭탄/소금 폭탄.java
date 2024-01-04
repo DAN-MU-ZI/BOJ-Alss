057db74292c7c3b7a09562e050d6f6b3918bba67
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-04 23:33:41 +0900

    [Bronze III] Title: 소금 폭탄, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
+	static final int DAY_TO_SECONDS = 86400;
+	static final int HOUR_TO_SECONDS = 3600;
+	static final int MINUTE_TO_SECONDS = 60;
+
 	static int StringTimeToSeconds(String string) {
 		String[] times = string.split(":");
 
 		int timestamp;
-		timestamp = Integer.parseInt(times[0]);
-		timestamp = timestamp * 60 + Integer.parseInt(times[1]);
-		timestamp = timestamp * 60 + Integer.parseInt(times[2]);
+		timestamp = Integer.parseInt(times[0]) * HOUR_TO_SECONDS;
+		timestamp = timestamp + Integer.parseInt(times[1]) * MINUTE_TO_SECONDS;
+		timestamp = timestamp + Integer.parseInt(times[2]);
 
 		return timestamp;
 	}
 
 	static String SecondsToStringTime(int seconds) {
-		int HH = seconds / (60 * 60);
-		int MM = seconds / 60 % 60;
-		int SS = seconds % 60;
+		int HH = seconds / HOUR_TO_SECONDS;
+		int MM = seconds / MINUTE_TO_SECONDS % MINUTE_TO_SECONDS;
+		int SS = seconds % MINUTE_TO_SECONDS;
 		return String.format("%02d:%02d:%02d", HH, MM, SS);
 	}
 
@@ -28,12 +32,11 @@ public class Main {
 		int startSeconds = StringTimeToSeconds(startTime);
 		int endSeconds = StringTimeToSeconds(endTime);
 
-		String string;
-		if (endSeconds > startSeconds) {
-			string = SecondsToStringTime(endSeconds - startSeconds);
-		} else {
-			string = SecondsToStringTime(endSeconds + (24 * 60 * 60) - startSeconds);
+		int timeDiff = endSeconds - startSeconds;
+		if (timeDiff <= 0) {
+			timeDiff = timeDiff + DAY_TO_SECONDS;
 		}
-		System.out.println(string);
+		String answer = SecondsToStringTime(timeDiff);
+		System.out.println(answer);
 	}
 }
