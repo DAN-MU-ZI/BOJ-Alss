bbbce0831aaf8cb35b57386a5b7d10930b5dd1a7
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-06 18:01:14 +0900

    [Bronze I] Title: 유레카 이론, Time: 304 ms, Memory: 18340 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -9,22 +9,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int t = Integer.parseInt(br.readLine());
-		int[] table = new int[44];
-		int s = 0;
-		for (int i = 1; i < 1000; i++) {
-			if (s + i > 1000) {
-				break;
-			}
-			s = s + i;
-			table[i - 1] = s;
+		int[] table = new int[45];
+		for (int i = 1; i <= 44; i++) {
+			table[i] = table[i - 1] + i;
 		}
 
 		int[] flagTable = new int[1001];
-		for (int i : table) {
-			for (int j : table) {
-				for (int k : table) {
-					if (i+j+k<=1000)
-						flagTable[i + j + k] = 1;
+		for (int i = 1; i < table.length; i++) {
+			for (int j = 1; j < table.length; j++) {
+				for (int k = 1; k < table.length; k++) {
+					int sum = table[i] + table[j] + table[k];
+					if (sum <= 1000)
+						flagTable[sum] = 1;
 				}
 			}
 		}
