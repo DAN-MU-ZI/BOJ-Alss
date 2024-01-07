f9c1ae9732ce8fdd7b6b7ba697e44f1d1b283b82
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-08 02:28:59 +0900

    [Silver V] Title: 줄세우기, Time: 304 ms, Memory: 20060 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,35 +1,40 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static int[] arr;
+	static int answer;
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int p = Integer.parseInt(br.readLine());
 		for (int i = 0; i < p; i++) {
-			func(br);
-		}
-		System.out.println(sb);
-	}
+			st = new StringTokenizer(br.readLine());
+			st.nextToken();
 
-	static void func(BufferedReader br) throws IOException {
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int caseNum = Integer.parseInt(st.nextToken());
-		int[] arr = new int[20];
-		int answer = 0;
-		for (int i = 0; i < 20; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < i; j++) {
-				if (arr[i] < arr[j]) {
-					answer++;
+			arr = new int[20];
+			answer = 0;
+			for (int k = 0; k < 20; k++) {
+				arr[k] = Integer.parseInt(st.nextToken());
+			}
+			for (int k = 0; k < 20; k++) {
+				for (int j = 0; j < k; j++) {
+					if (arr[k] < arr[j]) {
+						answer++;
+					}
 				}
 			}
+			sb.append(String.format("%d %d\n", i + 1, answer));
 		}
-		sb.append(String.format("%d %d\n", caseNum, answer));
+		bw.write(String.valueOf(sb));
+		bw.close();
 	}
+
 }
