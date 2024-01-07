9578131604410a2087f847e3bb9ecd71adb8c9df
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-08 01:48:55 +0900

    [Silver III] Title: ALPS식 투표, Time: 132 ms, Memory: 14460 KB -BaekjoonHub

---
+++ 백준/Silver/2817. ALPS식 투표/ALPS식 투표.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int X = Integer.parseInt(br.readLine());
+		int cutline = X / 100 * 5;
+		int N = Integer.parseInt(br.readLine());
+		int[][] scores = new int[N][15];
+		int[] members = new int[26];
+		boolean[] visited = new boolean[26];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int c = st.nextToken().charAt(0);
+			int vote = Integer.parseInt(st.nextToken());
+			if (vote < cutline) {
+				continue;
+			}
+			for (int j = 1; j <= 14; j++) {
+				scores[i][j] = vote / j;
+			}
+			members[c - 'A'] = i;
+			visited[c - 'A'] = true;
+		}
+
+		int[] chips = new int[N];
+		for (int k = 0; k < 14; k++) {
+			int nthMaxvalue = 0;
+			int x = 0;
+			int y = 0;
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 1; j <= 14; j++) {
+					if (nthMaxvalue < scores[i][j]) {
+						nthMaxvalue = scores[i][j];
+						x = i;
+						y = j;
+					}
+				}
+			}
+			scores[x][y] = 0;
+			chips[x]++;
+		}
+
+		for (char c = 'A'; c <= 'Z'; c++) {
+			if (visited[c - 'A']) {
+				System.out.printf("%c %d\n", c, chips[members[c - 'A']]);
+			}
+		}
+	}
+}
