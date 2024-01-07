db5ef0768767ed58f60b334fa2be04b7ffc497a3
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-08 02:23:00 +0900

    [Silver V] Title: 줄세우기, Time: 340 ms, Memory: 23096 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,16 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int p = Integer.parseInt(br.readLine());
 		for (int i = 0; i < p; i++) {
 			func(br);
 		}
+		System.out.println(sb);
 	}
 
 	static void func(BufferedReader br) throws IOException {
@@ -19,14 +21,15 @@ public class Main {
 		int[] arr = new int[20];
 		int answer = 0;
 		for (int i = 0; i < 20; i++) {
-			int n = Integer.parseInt(st.nextToken());
-			arr[i] = n;
-			for (int j = i - 1; j >= 0; j--) {
-				if (arr[j] > arr[i]) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[i] < arr[j]) {
 					answer++;
 				}
 			}
 		}
-		System.out.printf("%d %d\n", caseNum, answer);
+		sb.append(String.format("%d %d\n", caseNum, answer));
 	}
 }
