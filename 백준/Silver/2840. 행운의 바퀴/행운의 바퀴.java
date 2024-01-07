af3a1fb86f932eeb45026db56af4724f2ae0a076
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-07 23:50:02 +0900

    [Silver IV] Title: 행운의 바퀴, Time: 140 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		boolean[] history = new boolean[26];
+		int[] board = new int[n];
+		int idx = 0;
+
+		for (int i = 0; i < k; i++) {
+			st = new StringTokenizer(br.readLine());
+			int term = Integer.parseInt(st.nextToken());
+			int c = st.nextToken().charAt(0);
+			idx = (idx + term) % n;
+
+			if ((board[idx] != 0 && board[idx] != c)
+				|| (board[idx] == 0 && history[c - 'A'])) {
+				System.out.println("!");
+				return;
+			}
+			board[idx] = c;
+			history[c - 'A'] = true;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (board[idx] == 0) {
+				System.out.print("?");
+			} else {
+				System.out.printf("%c", board[idx]);
+			}
+			idx = (--idx + n) % n;
+		}
+	}
+}
