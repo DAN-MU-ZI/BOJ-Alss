7f6ba74b8406ab6519150cfa96bbe65283548322
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-10 04:19:37 +0900

    [Silver IV] Title: 대칭 차집합, Time: 688 ms, Memory: 249604 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n, m;
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		boolean[] a = new boolean[100000001];
+		boolean[] b = new boolean[100000001];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			a[Integer.parseInt(st.nextToken())] = true;
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < m; i++) {
+			b[Integer.parseInt(st.nextToken())] = true;
+		}
+
+		int answer = 0;
+		for (int i = 1; i < 100000001; i++) {
+			if (a[i])
+				answer++;
+			if (b[i])
+				answer++;
+			if (a[i] && b[i]) {
+				answer--;
+				answer--;
+			}
+		}
+		System.out.println(answer);
+	}
+}
