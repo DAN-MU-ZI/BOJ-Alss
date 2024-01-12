3632713d7eba92a6a2646cacd760df70451366b2
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-12 21:57:15 +0900

    [Silver II] Title: Generic Queries, Time: 1560 ms, Memory: 307876 KB -BaekjoonHub

---
+++ 백준/Silver/16713. Generic Queries/Generic Queries.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n, m;
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n + 1];
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 1; i <= n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			arr[i] = num ^ arr[i - 1];
+		}
+
+		int answer = 0;
+		for (int j = 0; j < m; j++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			answer ^= arr[b] ^ arr[a - 1];
+		}
+		System.out.println(answer);
+	}
+}
