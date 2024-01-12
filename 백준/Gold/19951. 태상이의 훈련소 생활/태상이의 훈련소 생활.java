c47b8482af8b07795961e196a30947da77b9cb8e
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-01-13 04:07:58 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 2508 ms, Memory: 70536 KB -BaekjoonHub

---
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n, m;
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			for (int j = a - 1; j < b; j++) {
+				arr[j] += k;
+			}
+		}
+
+		System.out.println(Arrays.stream(arr)
+			.mapToObj(String::valueOf)
+			.collect(Collectors.joining(" ")));
+	}
+}
