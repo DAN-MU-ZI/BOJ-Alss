e3f00cb3f0309bce20213a007bc5fd576a1d8e66
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-15 20:20:03 +0900

    [Silver IV] Title: 정수 제곱근, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine());
+		System.out.println(func(n));
+	}
+
+	private static long func(long n) {
+		if (n == 0)
+			return 0;
+		long s = 1, e = 1L << 32, res = -1;
+		while (s <= e) {
+			long m = (s + e) / 2;
+			if (m >= (n - 1) / m + 1) {
+				e = m - 1;
+				res = m;
+			} else
+				s = m + 1;
+		}
+		return res;
+	}
+}
