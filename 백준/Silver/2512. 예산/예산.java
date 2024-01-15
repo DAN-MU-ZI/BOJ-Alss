a821611dda3a2a2f918f70ca30dd660269c5c745
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-16 06:04:17 +0900

    [Silver II] Title: 예산, Time: 184 ms, Memory: 15452 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			arr[i] = num;
+		}
+
+		int m = Integer.parseInt(br.readLine());
+
+		int s = 0;
+		int e = Arrays.stream(arr).max().getAsInt();
+		int answer = 0;
+		while (s <= e) {
+			int mid = (s + e) / 2;
+			int sum = 0;
+			for (int a : arr) {
+				sum += Math.min(mid, a);
+			}
+
+			if (m >= sum) {
+				s = mid + 1;
+				answer = mid;
+			} else
+				e = mid - 1;
+		}
+		System.out.println(answer);
+	}
+}
