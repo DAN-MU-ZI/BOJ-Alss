333342ceab4061e2549b800821ba603edbb0b500
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-17 13:59:54 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 748 ms, Memory: 94076 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int RANGE = 2000001;
 		int SHIFT = 1000000;
@@ -18,11 +15,12 @@ public class Main {
 			arr[Integer.parseInt(br.readLine()) + SHIFT] = true;
 		}
 
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < RANGE; i++) {
 			if (arr[i]) {
-				bw.write(i - SHIFT + "\n");
+				sb.append(i - SHIFT).append("\n");
 			}
 		}
-		bw.close();
+		System.out.println(sb);
 	}
 }
