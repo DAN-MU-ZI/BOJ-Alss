a7a486f7746a2602875015d159e205d7099c7c88
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-19 16:21:12 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 348 ms, Memory: 25080 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		char[][] arr = new char[N + 1][M + 1];
+		for (int i = 1; i < N + 1; i++) {
+			String line = br.readLine();
+			for (int j = 1; j < M + 1; j++) {
+				arr[i][j] = line.charAt(j - 1);
+			}
+		}
+
+		int[][] acc = new int[N + 1][M + 1];
+		for (int i = 1; i < N + 1; i++) {
+			for (int j = 1; j < M + 1; j++) {
+				if (arr[i][j] == '1') {
+					acc[i][j] = Math.min(acc[i - 1][j - 1], Math.min(acc[i][j - 1], acc[i - 1][j])) + 1;
+				} else {
+					acc[i][j] = 0;
+				}
+			}
+		}
+		// for (int i = 0; i < N + 1; i++) {
+		// 	System.out.println(Arrays.toString(acc[i]));
+		// }
+
+		int answer = 0;
+		for (int i = 1; i < N + 1; i++) {
+			for (int j = 1; j < M + 1; j++) {
+				answer = Math.max(answer, acc[i][j] * acc[i][j]);
+			}
+		}
+		System.out.println(answer);
+	}
+}
