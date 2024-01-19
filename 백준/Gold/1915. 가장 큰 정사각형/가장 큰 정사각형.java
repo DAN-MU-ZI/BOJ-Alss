7794581c32006ea86835d0ef35cb8779bacc05a9
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-19 16:21:44 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 344 ms, Memory: 25824 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -29,16 +29,13 @@ public class Main {
 				}
 			}
 		}
-		// for (int i = 0; i < N + 1; i++) {
-		// 	System.out.println(Arrays.toString(acc[i]));
-		// }
 
 		int answer = 0;
 		for (int i = 1; i < N + 1; i++) {
 			for (int j = 1; j < M + 1; j++) {
-				answer = Math.max(answer, acc[i][j] * acc[i][j]);
+				answer = Math.max(answer, acc[i][j]);
 			}
 		}
-		System.out.println(answer);
+		System.out.println(answer*answer);
 	}
 }
