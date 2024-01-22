b9225875b76c5fb33ab0b2f75ea0c10010b5fef6
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-22 17:25:34 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 368 ms, Memory: 20496 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String line = br.readLine();
+			char[] arr = line.toCharArray();
+
+			if (line.charAt(0) == '.')
+				break;
+
+			Stack<Character> stk = new Stack<>();
+			boolean result = true;
+			for (int i = arr.length - 1; i >= 0; i--) {
+				char c = arr[i];
+				if (c == '(' || c == ')' || c == '[' || c == ']') {
+					if (stk.isEmpty()) {
+						if (c == ')' || c == ']')
+							stk.push(c);
+						else {
+							result = false;
+							break;
+						}
+					} else {
+						if ((stk.peek() == ')' && c == '[') || stk.peek() == ']' && c == '(') {
+							result = false;
+							break;
+						} else {
+							if ((c == '(' && stk.peek() == ')') || (c == '[' && stk.peek() == ']'))
+								stk.pop();
+							else
+								stk.push(c);
+						}
+					}
+				}
+			}
+			if (!stk.isEmpty())
+				result = false;
+
+			System.out.println(result ? "yes" : "no");
+		}
+
+	}
+}
