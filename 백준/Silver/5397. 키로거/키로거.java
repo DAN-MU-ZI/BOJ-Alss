1bc045fa5cc987ccd34a464c793ae358e0f2d5bb
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-22 22:22:25 +0900

    [Silver II] Title: 키로거, Time: 1092 ms, Memory: 160892 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		while (T-- > 0) {
+			char[] charArray = br
+				.readLine()
+				.toCharArray();
+			Stack<Character> stk = new Stack<>();
+			Stack<Character> tmp = new Stack<>();
+
+			final char BACKSPACE = '-';
+			final char LEFT_ARROW = '<';
+			final char RIGHT_ARROW = '>';
+			for (char c : charArray) {
+				switch (c) {
+					case BACKSPACE:
+						if (!stk.isEmpty())
+							stk.pop();
+						break;
+					case LEFT_ARROW:
+						if (!stk.isEmpty())
+							tmp.push(stk.pop());
+						break;
+					case RIGHT_ARROW:
+						if (!tmp.isEmpty())
+							stk.push(tmp.pop());
+						break;
+					default:
+						stk.push(c);
+						break;
+				}
+			}
+
+			StringBuilder sb = new StringBuilder();
+			stk.forEach(sb::append);
+			
+			Collections.reverse(tmp);
+			tmp.forEach(sb::append);
+			System.out.println(sb);
+		}
+	}
+}
