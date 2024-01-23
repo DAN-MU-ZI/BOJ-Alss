81888f59df6ffff4d12fee9e873d077e6656139b
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-23 10:45:22 +0900

    [Gold III] Title: 크게 만들기, Time: 1812 ms, Memory: 54700 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		Stack<Integer> stk = new Stack<>();
+		for (char c : br
+			.readLine()
+			.toCharArray()) {
+			int num = c - '0';
+			while (!stk.isEmpty() && K > 0 && stk.peek() < num) {
+				stk.pop();
+				K--;
+			}
+			stk.push(num);
+		}
+
+		while (K-- > 0) {
+			stk.pop();
+		}
+		for (int i : stk) {
+			System.out.print(i);
+		}
+	}
+}
