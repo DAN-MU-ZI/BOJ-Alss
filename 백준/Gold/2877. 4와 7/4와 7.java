48099c2973b5c13d1f48e31725fa45a830aa0e2d
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-23 20:47:59 +0900

    [Gold V] Title: 4와 7, Time: 120 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Gold/2877. 4와 7/4와 7.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int K = Integer.parseInt(br.readLine()) + 1;
+
+		StringBuilder sb = new StringBuilder();
+		while (K > 1) {
+			if (K % 2 == 0)
+				sb.append(4);
+			else
+				sb.append(7);
+			K /= 2;
+		}
+		System.out.println(sb.reverse());
+	}
+}
