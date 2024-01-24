2ff82e973aeaff848a568e3e7b494b8327c6c51f
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-01-24 15:06:13 +0900

    [Gold V] Title: 암호 만들기, Time: 156 ms, Memory: 16708 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static List<Character> vowels = List.of('a', 'e', 'i', 'o', 'u');
+	static List<Character> consonants = List.of('b', 'c', 'd', 'f', 'g', 'h', 'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+		's', 't', 'v', 'x', 'y', 'z');
+	static int L;
+	static int C;
+	static char[] arr;
+	static boolean[] visited;
+	static int vowelCnt = 0;
+	static int consonantCnt = 0;
+
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		arr = new char[C];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < C; i++) {
+			arr[i] = st
+				.nextToken()
+				.charAt(0);
+		}
+		Arrays.sort(arr);
+
+		visited = new boolean[C];
+
+		func(0);
+		System.out.println(sb);
+	}
+
+	private static void func(int depth) {
+		if (depth == C) {
+			if (vowelCnt > 0 && consonantCnt > 1 && vowelCnt + consonantCnt == L) {
+				for (int i = 0; i < C; i++)
+					if (visited[i])
+						sb.append(arr[i]);
+				sb.append("\n");
+			}
+			return;
+		}
+
+		visited[depth] = true;
+		if (vowels.contains(arr[depth]))
+			vowelCnt++;
+		else
+			consonantCnt++;
+		func(depth + 1);
+		visited[depth] = false;
+		if (vowels.contains(arr[depth]))
+			vowelCnt--;
+		else
+			consonantCnt--;
+		func(depth + 1);
+	}
+}
