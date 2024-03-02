27eef3e88c8484bf17d7db3d35511ccbd22db110
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-03-02 18:10:55 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -4,24 +4,18 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int r;
-    private static int c;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
         int size = (int) Math.pow(2, n);
 
         int row = 0, col = 0;
         int answer = 0;
         while (!(row == r && col == c)) {
-//            System.out.println("row = " + row);
-//            System.out.println("col = " + col);
-//            System.out.println("answer = " + answer);
             size /= 2;
 
             boolean rowCon = r >= row + size;
@@ -31,10 +25,10 @@ public class Main {
                 row += size;
                 col += size;
                 answer += size * size * 3;
-            } else if (rowCon && !colCon) {
+            } else if (rowCon) {
                 row += size;
                 answer += size * size * 2;
-            } else if (!rowCon && colCon) {
+            } else if (colCon) {
                 col += size;
                 answer += size * size;
             }
