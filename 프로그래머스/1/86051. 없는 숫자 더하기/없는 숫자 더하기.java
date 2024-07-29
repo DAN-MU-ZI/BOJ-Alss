371eb77ffe2f187bfacf6198b4433b36a042f850
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-07-29 11:19:34 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,16 +1,8 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = -1;
-        
-        boolean[] table = new boolean[10];
-        for(int i: numbers){
-            table[i]=true;
-        }
-        
-        int sum = 0;
-        for(int i=0;i<table.length;i++){
-            if(!table[i])
-                sum+=i;
+        int sum = 45;
+        for(int i:numbers){
+            sum-=i;
         }
         
         return sum;
