0ed69e46527f4b65ca1a36fd622612996ce37aa1
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-11-26 20:17:43 +0900

    [Bronze I] Title: 이항 계수 1, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+answer = 1
+for i in range(N - K + 1, N + 1):
+    answer *= i
+for i in range(1, K + 1):
+    answer = answer // i
+print(answer)
