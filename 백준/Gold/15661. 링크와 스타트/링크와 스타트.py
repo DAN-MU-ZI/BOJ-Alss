a42432a849a62ca66eb5b684e438c38bb7488e0d
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-12-24 20:43:33 +0900

    [Gold V] Title: 링크와 스타트, Time: 980 ms, Memory: 127124 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.py
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.py
@@ -6,15 +6,15 @@ N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
 
-def get_score_bitmask(mask):
+scores = [0] * (1 << N + 1)
+for mask in range(1 << N):
     score = 0
     for x in range(N):
         if mask & (1 << x):
             for y in range(x + 1, N):
                 if mask & (1 << y):
                     score += arr[x][y] + arr[y][x]
-    return score
-
+    scores[mask] = score
 
 mask_full = (1 << N) - 1
 answer = sys.maxsize
@@ -22,8 +22,8 @@ answer = sys.maxsize
 for subset in range(1, 1 << (N - 1)):
     complement = mask_full ^ subset
 
-    s1 = get_score_bitmask(subset)
-    s2 = get_score_bitmask(complement)
+    s1 = scores[subset]
+    s2 = scores[complement]
     answer = min(answer, abs(s1 - s2))
 
 print(answer)
