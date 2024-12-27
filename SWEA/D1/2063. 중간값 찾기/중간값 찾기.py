0336a057e75dc97f1f8490c1e812e91a41b484a6
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2024-12-28 00:52:45 +0900

    [D1] Title: 중간값 찾기, Time: 132 ms, Memory: 43,776 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
@@ -1,4 +1,23 @@
-N = int(input())
-arr = list(map(int, input().split()))
-arr.sort()
-print(arr[N//2])
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+class Solution
+{
+	public static void main(String args[]) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[N / 2]);
+	}
+}
