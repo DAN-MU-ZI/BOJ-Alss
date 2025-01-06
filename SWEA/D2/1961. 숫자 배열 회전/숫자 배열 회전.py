965cd53734e95ffdee427131333d97e03aedadee
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2025-01-06 19:37:40 +0900

    [D2] Title: 숫자 배열 회전, Time: 120 ms, Memory: 44,024 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,34 +1,48 @@
-T = int(input())
+import java.io.*;
+import java.util.*;
 
-for test_case in range(1, T + 1):
-    N = int(input())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    print(f'#{test_case}')
-    
-    result = [[0] * 3 * (N) for _ in range(N)]
-    
-    col_shift = 0
-    # rotate 90
-    for i in range(N):
-        line = arr[i]
-        for j in range(N):
-            result[j][N-1-i]=line[j]
-    # rotate 180
-    col_shift += N
-    for i in range(N):
-        line = arr[i]
-        for j in range(N):
-            result[N-1-i][col_shift+N-1-j]=line[j]
-    # rotate 270
-    col_shift += N
-    for i in range(N):
-        line = arr[i]
-        for j in range(N):
-            result[N-1-j][col_shift+i]=line[j]
-            
-    for line in result:
-        buffer = []
-        for i in range(3):
-            sub = [str(x) for x in line[i*N:(i+1)*N]]
-            buffer.append(''.join(sub))
-        print(' '.join(buffer))
+class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.parseInt(br.readLine());
+
+			int[][] arr = new int[N][N];
+
+			for (int r = 0; r < N; r++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int c = 0; c < N; c++) {
+					arr[r][c] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			sb.append(String.format("#%d\n", test_case));
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					sb.append(arr[N - c - 1][r]);
+				}
+				sb.append(" ");
+
+				for (int c = 0; c < N; c++) {
+					sb.append(arr[N - r - 1][N - c - 1]);
+				}
+				sb.append(" ");
+
+				for (int c = 0; c < N; c++) {
+					sb.append(arr[c][N - r - 1]);
+				}
+				sb.append("\n");
+			}
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
