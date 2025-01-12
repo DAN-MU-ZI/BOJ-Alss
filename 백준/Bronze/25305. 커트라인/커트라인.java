41dec9083e40255d5f6f36a0970190177d3b8359
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2025-01-12 13:02:48 +0900

    [Bronze II] Title: 커트라인, Time: 120 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		bw.write(String.format("%d\n", arr[N - K]));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
