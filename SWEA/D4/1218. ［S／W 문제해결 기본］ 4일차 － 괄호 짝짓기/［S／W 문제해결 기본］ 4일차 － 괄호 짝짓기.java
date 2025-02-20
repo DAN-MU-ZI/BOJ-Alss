f8cbd31b892a5a5f5019a0147b19e0917f977c4c
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2025-02-20 11:18:35 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 86 ms, Memory: 26,112 KB -BaekjoonHub

---
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int N;
+    static char[] line;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        final int T = 10;
+        for (int t = 1; t <= T; t++) {            
+            N = Integer.parseInt(br.readLine());
+            line = br.readLine().trim().toCharArray();
+
+            int answer = solve();
+            sb.append(String.format("#%d %s\n", t, answer));
+        }
+        
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    static int solve() {
+        Stack<Character> stk  = new Stack<>();
+
+        for (int i = N - 1; i >= 0; i--) {
+            char c = line[i];
+
+            if (stk.isEmpty()) {
+                stk.push(c);
+                continue;
+            }
+            
+            if (c == ')' || c == '}' || c == ']' || c == '>') {
+                stk.push(c);
+            } else {
+                if (
+                    c == '(' && stk.peek() != ')'
+                    || c == '[' && stk.peek() != ']'
+                    || c == '{' && stk.peek() != '}'
+                    || c == '<' && stk.peek() != '>'
+                ) {
+                    return 0;
+                } else {
+                    stk.pop();
+                }
+            }
+        }
+
+        return 1;
+    }
+}
