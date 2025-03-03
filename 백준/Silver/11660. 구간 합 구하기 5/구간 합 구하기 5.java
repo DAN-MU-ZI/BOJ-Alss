ac965c287c6b9b55ac67ad910c9c08d67f102843
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2025-03-03 19:02:12 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 832 ms, Memory: 129944 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,35 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+	import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	static int N, M;
+	static int[][] arr = new int[1025][1025];
+	static int[][] dp = new int[1025][1025];
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 
-		int n, m;
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+		StringTokenizer st;
 
-		int[][] arr = new int[n + 1][n + 1];
-		for (int i = 1; i <= n; i++) {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= n; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				arr[i][j] = arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1] + num;
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		StringBuilder sb = new StringBuilder();
-		for (int j = 0; j < m; j++) {
+		init();
+
+		for (int t = 0; t < M; t++) {
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-			sb.append(arr[x2][y2] + arr[x1 - 1][y1 - 1] - arr[x2][y1 - 1] - arr[x1 - 1][y2]).append("\n");
+			sb.append(solve(x1, y1, x2, y2) + "\n");
 		}
-		System.out.println(sb);
+
+		bw.write(sb.toString());
+		bw.close();
+		br.close();
+	}
+
+	static void init() {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i - 1][j - 1];
+			}
+		}
+	}
+
+	static int solve(int x1, int y1, int x2, int y2) {
+		return dp[x2][y2] + dp[x1 - 1][y1 - 1] - dp[x2][y1 - 1] - dp[x1 - 1][y2];
 	}
 }
