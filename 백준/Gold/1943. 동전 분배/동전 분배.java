a15ca4fed29e81e4c0179e7ba25e5379e08d5222
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2025-03-26 10:36:38 +0900

    [Gold II] Title: 동전 분배, Time: 252 ms, Memory: 23376 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -8,31 +8,19 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         
         int N;
+        int[][] coins;
         int[] dp;
         
         for (int t = 0; t < 3; t++) {
             N = Integer.parseInt(br.readLine());
+            coins = new int[N][2];
             int tot = 0;
-            dp = new int[500001];
+            
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 int coin = Integer.parseInt(st.nextToken());
                 int count = Integer.parseInt(st.nextToken());
-                for (int j = 0; j <= count; j++) {
-                	for (int k = tot; k >= coin; k--) {
-                		if (k + (coin * j) > 50000) {
-                			break;
-                		}
-                		
-                		if (dp[k] > 0) {
-                			dp[k + (coin * j)] += 1;
-                		}
-                	}
-                }
-                
-                for (int j = 0; j <= count; j++) {
-                	dp[coin * j] += 1;
-                }
+                coins[i] = new int[] {coin, count};
                 
                 tot += coin * count;
             }
@@ -42,9 +30,26 @@ public class Main {
                 continue;
             }
             
+            dp = new int[500001];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
             
+            for (int i = 0; i < N; i++) {
+            	int coin = coins[i][0];
+            	int count = coins[i][1];
+            	
+            	for (int k = 0; k <= tot / 2; k++) {
+        			if (dp[k] == Integer.MAX_VALUE) continue;
+        			
+        			if (k + coin <= tot / 2 && dp[k] < count) {
+        				dp[k + coin] = Math.min(dp[k + coin], dp[k] + 1);
+        			}
+        			
+        			dp[k] = 0;
+            	}
+            }
             
-            sb.append(dp[tot / 2] != 0? 1 : 0).append("\n");
+            sb.append(dp[tot / 2] == Integer.MAX_VALUE ? 0 : 1).append("\n");
         }
         System.out.println(sb);
     }
