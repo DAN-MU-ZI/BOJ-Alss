bd0e86806a318871c7a61afd91e7c15979b97446
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2026-01-09 06:46:37 +0900

    [Gold I] Title: 2048 (Easy), Time: 152 ms, Memory: 18692 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -34,20 +34,31 @@ public class Main {
         for (int i = 0; i < 4; i++) {
             int[][] nextBoard = new int[N][N];
             for (int r = 0; r < N; r++) {
-                nextBoard[r] = currentBoard[r].clone();
+                System.arraycopy(currentBoard[r], 0, nextBoard[r], 0, N);
             }
 
             move(i, nextBoard);
+
+            if (isSame(currentBoard, nextBoard)) continue;
 
             findMax(nextBoard);
             dfs(depth + 1, nextBoard);
         }
     }
 
+    static boolean isSame(int[][] a, int[][] b) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (a[i][j] != b[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
     static void findMax(int[][] board) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                maxBlock = Math.max(maxBlock, board[i][j]);
+                if (board[i][j] > maxBlock) maxBlock = board[i][j];
             }
         }
     }
@@ -55,68 +66,76 @@ public class Main {
     static void move(int dir, int[][] board) {
         if (dir == 0) {
             for (int j = 0; j < N; j++) {
-                Queue<Integer> q = new LinkedList<>();
+                int index = 0;
+                int prev = 0;
                 for (int i = 0; i < N; i++) {
-                    if (board[i][j] != 0) q.offer(board[i][j]);
-                    board[i][j] = 0;
+                    if (board[i][j] != 0) {
+                        if (prev == board[i][j]) {
+                            board[index - 1][j] *= 2;
+                            prev = 0;
+                        } else {
+                            board[index][j] = board[i][j];
+                            prev = board[i][j];
+                            index++;
+                        }
+                    }
                 }
-                compressAndFill(q, board, 0, j, 1, 0);
+                for (int i = index; i < N; i++) board[i][j] = 0;
             }
         } else if (dir == 1) {
             for (int j = 0; j < N; j++) {
-                Queue<Integer> q = new LinkedList<>();
+                int index = N - 1;
+                int prev = 0;
                 for (int i = N - 1; i >= 0; i--) {
-                    if (board[i][j] != 0) q.offer(board[i][j]);
-                    board[i][j] = 0;
+                    if (board[i][j] != 0) {
+                        if (prev == board[i][j]) {
+                            board[index + 1][j] *= 2;
+                            prev = 0;
+                        } else {
+                            board[index][j] = board[i][j];
+                            prev = board[i][j];
+                            index--;
+                        }
+                    }
                 }
-                compressAndFill(q, board, N - 1, j, -1, 0);
+                for (int i = index; i >= 0; i--) board[i][j] = 0;
             }
         } else if (dir == 2) {
             for (int i = 0; i < N; i++) {
-                Queue<Integer> q = new LinkedList<>();
+                int index = 0;
+                int prev = 0;
                 for (int j = 0; j < N; j++) {
-                    if (board[i][j] != 0) q.offer(board[i][j]);
-                    board[i][j] = 0;
+                    if (board[i][j] != 0) {
+                        if (prev == board[i][j]) {
+                            board[i][index - 1] *= 2;
+                            prev = 0;
+                        } else {
+                            board[i][index] = board[i][j];
+                            prev = board[i][j];
+                            index++;
+                        }
+                    }
                 }
-                compressAndFill(q, board, i, 0, 0, 1);
+                for (int j = index; j < N; j++) board[i][j] = 0;
             }
         } else if (dir == 3) {
             for (int i = 0; i < N; i++) {
-                Queue<Integer> q = new LinkedList<>();
+                int index = N - 1;
+                int prev = 0;
                 for (int j = N - 1; j >= 0; j--) {
-                    if (board[i][j] != 0) q.offer(board[i][j]);
-                    board[i][j] = 0;
+                    if (board[i][j] != 0) {
+                        if (prev == board[i][j]) {
+                            board[i][index + 1] *= 2;
+                            prev = 0;
+                        } else {
+                            board[i][index] = board[i][j];
+                            prev = board[i][j];
+                            index--;
+                        }
+                    }
                 }
-                compressAndFill(q, board, i, N - 1, 0, -1);
+                for (int j = index; j >= 0; j--) board[i][j] = 0;
             }
-        }
-    }
-
-    static void compressAndFill(Queue<Integer> q, int[][] board, int r, int c, int dr, int dc) {
-        int prev = 0;
-
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-
-            if (prev == 0) {
-                prev = cur;
-            } else {
-                if (prev == cur) {
-                    board[r][c] = prev * 2;
-                    prev = 0;
-                    r += dr;
-                    c += dc;
-                } else {
-                    board[r][c] = prev;
-                    prev = cur;
-                    r += dr;
-                    c += dc;
-                }
-            }
-        }
-
-        if (prev != 0) {
-            board[r][c] = prev;
         }
     }
 }
