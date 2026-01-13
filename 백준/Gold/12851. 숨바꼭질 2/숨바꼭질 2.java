7f55e77375d421ad2dc9e50451c09fc6060c48c5
DAN-MU-ZI <dlwogns6818@pusan.ac.kr>
2026-01-14 02:42:54 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 208 ms, Memory: 23976 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -23,9 +23,16 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        if (N >= K) {
+            System.out.println(N - K);
+            System.out.println(1);
+            return;
+        }
+
         int[] bestScores = new int[100_001];
         int[] bestCnt = new int[100_001];
         Arrays.fill(bestScores, Integer.MAX_VALUE);
+
         Queue<Log> queue = new PriorityQueue<>();
 
         queue.add(new Log(N, 0));
@@ -35,45 +42,24 @@ public class Main {
         while (!queue.isEmpty()) {
             Log l = queue.poll();
 
-            int nextCnt = l.cnt + 1;
-            int nextPos = l.pos + 1;
-            if (0 <= nextPos && nextPos <= 100_000) {
-                if (nextCnt < bestScores[nextPos]) {
-                    bestScores[nextPos] = nextCnt;
-                    bestCnt[nextPos] = 1;
-                    queue.add(new Log(nextPos, nextCnt));
-                }
-                else if (nextCnt == bestScores[nextPos]) {
-                    bestCnt[nextPos]++;
-                    queue.add(new Log(nextPos, nextCnt));
-                }
-            }
-            nextPos = l.pos - 1;
-            if (0 <= nextPos && nextPos <= 100_000) {
-                if (nextCnt < bestScores[nextPos]) {
-                    bestScores[nextPos] = nextCnt;
-                    bestCnt[nextPos] = 1;
-                    queue.add(new Log(nextPos, nextCnt));
-                }
-                else if (nextCnt == bestScores[nextPos]) {
-                    bestCnt[nextPos]++;
-                    queue.add(new Log(nextPos, nextCnt));
-                }
-            }
-            nextPos = l.pos * 2;
-            if (0 <= nextPos && nextPos <= 100_000) {
-                if (nextCnt < bestScores[nextPos]) {
-                    bestScores[nextPos] = nextCnt;
-                    bestCnt[nextPos] = 1;
-                    queue.add(new Log(nextPos, nextCnt));
-                }
-                else if (nextCnt == bestScores[nextPos]) {
-                    bestCnt[nextPos]++;
-                    queue.add(new Log(nextPos, nextCnt));
+            if (l.cnt > bestScores[l.pos]) continue;
+
+            int[] nextPositions = {l.pos + 1, l.pos - 1, l.pos * 2};
+
+            for (int nextPos : nextPositions) {
+                if (nextPos < 0 || nextPos > 100_000) continue;
+
+                int nextTime = l.cnt + 1;
+
+                if (nextTime < bestScores[nextPos]) {
+                    bestScores[nextPos] = nextTime;
+                    bestCnt[nextPos] = bestCnt[l.pos];
+                    queue.add(new Log(nextPos, nextTime));
+                } else if (nextTime == bestScores[nextPos]) {
+                    bestCnt[nextPos] += bestCnt[l.pos];
                 }
             }
         }
-
 
         System.out.println(bestScores[K]);
         System.out.println(bestCnt[K]);
